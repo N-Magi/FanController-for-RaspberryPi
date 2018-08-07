586ebdd9521574d8983c4ac69ee4726f4c3ef9ef
野平真義 <masayoshi55@live.jp>
2018-08-07 22:06:02 +0900

    設定ファイル、ログファイルを出力するようにしました

--- FanController/Program.cs
+++ FanController/Program.cs
@@ -8,31 +8,63 @@ namespace FanController
     {
 
         static IConfiguration config;
+        static Model.Logger HWlogger = null;
+        static Model.Logger FanLogger = null;
         //static Model.PWM pwm;
 
         static void Main(string[] args)
         {
             config = configs.Configuration.GetConfiguration();
 
+            HWlogger = new Model.Logger(@"/var/log/FanController","HWstatus.csv");
+            FanLogger = new Model.Logger(@"/var/log/FanController","log.log");
+
+            
+            Console.WriteLine("a");
+
+            var pinSection = config.GetSection("PinSettings");
+            int logicPin = int.Parse(pinSection["LogicPin"]);
+            int pwmPin = int.Parse(config["PinSettings:PwmPin"]);
+            var SpeedSet = config.GetSection("FanSpeedSettings");
+            var tempSet = config.GetSection("TemperatureSettings");
+
+            int maxSpeed = 100;
+            if (!int.TryParse(SpeedSet["MaxSpeed"], out maxSpeed)) return;
+            int minSpeed = 0;
+            if (!int.TryParse(SpeedSet["MinSpeed"], out minSpeed)) return;
+
+            int minTemp = 40;
+            if (!int.TryParse(tempSet["MinSpeedTemp"], out minTemp)) return;
+            int maxTemp = 55;
+            if (!int.TryParse(tempSet["MaxSpeedTemp"], out maxTemp)) return;
+
+
+
+
+
+            FanLogger.Print("Starting... RaspiFanController StartingSettings is...");
+            FanLogger.Print($@"PwmPin :{pwmPin} GpioLogicPin: {logicPin}");
+
+            FanLogger.Print("Load... FanSpeedSettings");
+            FanLogger.Print($"FanMaxSpeed Is {maxSpeed}  FanMinSpeed Is {minSpeed}");
+            FanLogger.Print($"FanMaxTemp Is {maxTemp} FanMinTemp Is {minTemp}");
+
 
 
             Boot();
-
-
             if (args.Length <= 0)
             {
                 while (true)
                 {
                     loop();
-
                 }
-                return;
             }
+
             int duty = 0;
-            //Console.WriteLine(args[0]);
             if (!int.TryParse(args[0], out duty)) return;
+
             Model.PWM pwm = new Model.PWM(100, duty);
-            Model.GPIO.PinMode(int.Parse(config["PinSettings:PwmPin"]), pwm, true);
+            Model.GPIO.PinMode(pwmPin, pwm, true);
 
         }
 
@@ -53,6 +85,18 @@ namespace FanController
             var SpeedSet = config.GetSection("FanSpeedSettings");
             var tempSet = config.GetSection("TemperatureSettings");
 
+            int maxSpeed = 100;
+            if (!int.TryParse(SpeedSet["MaxSpeed"], out maxSpeed)) return;
+            int minSpeed = 0;
+            if (!int.TryParse(SpeedSet["MinSpeed"], out minSpeed)) return;
+
+            int minTemp = 40;
+            if (!int.TryParse(tempSet["MinSpeedTemp"], out minTemp)) return;
+            int maxTemp = 55;
+            if (!int.TryParse(tempSet["MaxSpeedTemp"], out maxTemp)) return;
+
+            
+
             float temp = 0;
             if (!float.TryParse(File.ReadAllText("/sys/class/thermal/thermal_zone0/temp"), out temp)) return;
             temp = temp / 1000;
@@ -61,18 +105,18 @@ namespace FanController
             {
                 Console.WriteLine(temp);
             }
-            int max = int.Parse(SpeedSet["MaxSpeed"]);
-            int min = int.Parse(SpeedSet["MinSpeed"]);
+            
+            float perTemp = (temp - minTemp) / (maxTemp - minTemp);
+            Console.WriteLine(perTemp);
+            float perSpeed = minSpeed + ((maxSpeed - minSpeed) * perTemp);
+            
 
-            float perTemp = (temp - int.Parse(tempSet["MinSpeedTemp"])) / (int.Parse(tempSet["MaxSpeedTemp"]) - int.Parse(tempSet["MinSpeedTemp"]));
-            //Console.WriteLine(perTemp);
-            float perSpeed = int.Parse(SpeedSet["MinSpeed"]) + (int.Parse(SpeedSet["MaxSpeed"]) - int.Parse(SpeedSet["MinSpeed"])) * perTemp;
+            if (perTemp >= 1) perSpeed = maxSpeed;
+            if (perTemp <= 0) perSpeed = maxSpeed > minSpeed ? minSpeed : maxSpeed;
+            perSpeed = maxSpeed == minSpeed ? maxSpeed : perSpeed;
 
-            if (perTemp >= 1) perSpeed = max;
-            if (perTemp <= 0) perSpeed = max > min ? min : max;
-            perSpeed = (int.Parse(tempSet["MaxSpeedTemp"]) - int.Parse(tempSet["MinSpeedTemp"])) == 0 ? max : perSpeed;
+            HWlogger.PrintCSV($"{temp.ToString().PadRight(8)}  , {perSpeed.ToString().PadRight(9)}");
 
-            Console.WriteLine(perSpeed);
 
             Model.PWM pwm = new Model.PWM(100, (int)perSpeed);
             Model.GPIO.PinMode(int.Parse(config["PinSettings:PwmPin"]), pwm, true);
